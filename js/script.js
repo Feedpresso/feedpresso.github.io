972a122c9cdfaf95ea5045b3d1af124d4acd7dee
Tadas Subonis <tadas.subonis@gmail.com>
2016-12-07 12:02:48 +0100

    Fading text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 
 jQuery(document).ready(function ($) {
     'use strict';
+                    
+    (function() {
+
+        var quotes = $(".quotes");
+        var quoteIndex = -1;
+        
+        function showNextQuote() {
+            ++quoteIndex;
+            quotes.eq(quoteIndex % quotes.length)
+                .fadeIn(2000)
+                .delay(2000)
+                .fadeOut(2000, showNextQuote);
+        }
+        
+        showNextQuote();
+    
+    })();
 
     //jQuery for page scrolling feature - requires jQuery Easing plugin
     $('.page-scroll').on('click', function (event) {
